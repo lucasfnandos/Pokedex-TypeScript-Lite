fd8b9d0f4b83417e859d78e97fd71f8faa0730cf
lucasfnandos <lucasfsoares@hotmail.com>
2026-06-07 10:42:09 -0300

    chore: removido comentarios do codigo em boxservice

--- src/service/BoxService.ts
+++ src/service/BoxService.ts
@@ -6,23 +6,19 @@
     adicionar(pokemon: PokemonResumo): void { 
         const jaExiste = this.pokemons.some((item) => item.id ===  pokemon.id); 
         if (jaExiste) {
-            //avisoPokemonJaCadastrado 
             console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`); 
             return; 
         } 
         this.pokemons.push(pokemon);
-        //successPokemonAdicionado 
         console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`); 
     } 
     
     listar(): void { 
         if (this.pokemons.length === 0) {
-            //avisoCatalogoVazio
             console.log("[AVISO] Catálogo vazio."); 
             return; 
         } 
         this.pokemons.forEach((pokemon) => {
-            //dadosPokemonFormatado 
         console.log(`#${pokemon.id} - ${pokemon.nome} | Tipos: ${pokemon.tipos.join(",")}`); 
         }); 
     } 
@@ -30,12 +26,10 @@
     remover(id: number): void { 
         const existe = this.pokemons.some((pokemon) => pokemon.id === id); 
         if (!existe) {
-            //avisoPokemonNaoEncontrado 
         console.log("[AVISO] Nenhum Pokémon encontrado com esse ID."); 
         return;  
         } 
         this.pokemons = this.pokemons.filter((pokemon) => pokemon.id !== id);
-        //successPokemonRemovido 
         console.log("[OK] Pokémon removido do catálogo."); 
     } 
 }
