a245b1ea36be8b8bb8e2b2248e29e741de20b15a
lucasfnandos <lucasfsoares@hotmail.com>
2026-06-07 10:38:57 -0300

    feat: implementado a function de buscar Pokemon na API em service

---
+++ src/service/PokeApiService.ts
@@ -0,0 +1,29 @@
+import type { 
+    PokemonResumo, 
+    PokemonApiResponse 
+} from '../model/Pokemon.js';
+
+const url: string = 'https://pokeapi.co/api/v2/pokemon/'
+
+export async function buscarPokemon(nomeOuId: string): Promise<PokemonResumo | null> { 
+    // implementação
+    try { 
+        const resposta = await fetch(url+nomeOuId);
+        if (!resposta.ok) { 
+        console.log("[ERRO] Pokémon não encontrado."); 
+        return null; 
+        } 
+        const dados: PokemonApiResponse = await resposta.json();
+        const pokemonResumo = {
+            "id": dados.id,
+            "nome": dados.name,
+            "tipos": dados.types.map(item => item.type.name),
+            "altura": dados.height,
+            "peso": dados.weight
+        }
+        return pokemonResumo
+    } catch (erro) { 
+        console.log("[ERRO] Não foi possível buscar o Pokémon."); 
+        return null; 
+    }
+}
