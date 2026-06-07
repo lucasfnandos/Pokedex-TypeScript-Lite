5a0f897cb9bc2e6c35d0b158dd647e0779b2523f
lucasfnandos <lucasfsoares@hotmail.com>
2026-06-07 20:48:02 -0300

    feat: implementado main com as chamadas dos services através do controller

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,28 @@ import {
     CatalogoPokemon 
 } from './service/BoxService.js'
 
-function main(): void{
+async function main(): Promise<void>{
     
     const catalogo = new CatalogoPokemon()
     const controller = new Controller(catalogo)
 
-    controller.adicionarAoCatalogo("pikachu")
-    controller.adicionarAoCatalogo("pikachu")
+    await controller.buscarPokemon("pikachu")
+    await controller.buscarPokemon("foo-foo")
 
+    await controller.adicionarAoCatalogo("pikachu")
+    await controller.adicionarAoCatalogo("charmander")
+
+    controller.listarPokemons()
+
+    await controller.adicionarAoCatalogo("mewtwo")
+    await controller.adicionarAoCatalogo("charizard")
+
+    controller.removerDoCatalogo(25)
+    controller.removerDoCatalogo("foo-foo")
+    controller.removerDoCatalogo(67)
+
+    controller.listarPokemons()
+
+    
 }
 main()
